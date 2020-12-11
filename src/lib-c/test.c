5161e0df7f10180c3f9795fae1b1c3f5b9726182
lcy0x1 <arthurlcy0x1@gmail.com>
2020-12-10 16:32:37 -0800

    c-js and js-c calls

--- src/lib-c/test.c
+++ src/lib-c/test.c
@@ -1,5 +1,8 @@
 #include <webassembly.h>
 
+extern void consoleLog(int va, int vb);
+
 export int add(int a, int b){
+    consoleLog(a,b);
     return a+b;
 }
--- src/main/test.js
+++ src/main/test.js
@@ -1,8 +1,12 @@
 const wasm = require("webassembly");
 
 async function test(){
-	const file = await wasm.load("src/bin/test.wasm");
-	console.log("1 + 2 = "+ file.exports.add(1,2));
+	const file = await wasm.load("src/bin/test.wasm", {
+		imports: {
+			"consoleLog" : (a,b)=>console.log(`${a} + ${b} = `)
+		}
+	});
+	console.log(file.exports.add(1,2));
 }
 
 test();
