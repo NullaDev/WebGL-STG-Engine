a6e09116263795ad9477b3dd791d7ef375a6819f
lcy0x1 <arthurlcy0x1@gmail.com>
2020-12-12 03:10:42 -0800

    sample test unit

--- src/stg/schedule/Scheuler.ts
+++ src/stg/schedule/Scheuler.ts
@@ -42,6 +42,51 @@ class Adder extends ScheduleEntry {
 
 }
 
+export abstract class ScheduleSupplier {
+
+    public abstract supply(): Item[];
+
+}
+
+export type Input = number | (() => void) | ScheduleSupplier;
+
+type Item = ScheduleEntry | ScheduleSupplier;
+
+function parse(input: Input[]): Item[] {
+    return input.map(e =>
+        typeof e == "number" ? new Wait(<number>e) :
+            typeof e == "function" ? new Adder(<() => void>e) :
+                e);
+}
+
+export type Repeat = ((i?: number) => Input[]) | Input[];
+
+export class RepeatSupplier extends ScheduleSupplier {
+
+    todo: Repeat;
+    index: number = 0;
+    total: number;
+
+    constructor(input: Repeat, n: number) {
+        super();
+        this.todo = input;
+        this.total = n;
+    }
+
+    public supply(): Item[] {
+        if (this.index >= this.total)
+            return null;
+        var ans;
+        if (typeof this.todo == "function")
+            ans = this.todo(this.index);
+        else ans = this.todo;
+        this.index++;
+        return parse(ans);
+    }
+
+
+}
+
 export const template_config_scheduler: Config = {
     render_layer: RL_INVISIBLE,
     collide_group: CG_GHOST,
@@ -50,21 +95,34 @@ export const template_config_scheduler: Config = {
 
 export class Scheduler extends SINull implements Entity<Scheduler, null, null, null> {
 
-    public list: ScheduleEntry[];
+    public list: Item[];
     config: Config = template_config_scheduler;
     state: State = State.PRE_ENTRY;
 
-    constructor(input: (number | (() => void))[]) {
+    constructor(input: Input[]) {
         super();
-        this.list = input.map(e => isNaN(+e) ? new Adder(<() => void>e) : new Wait(<number>e));
+        this.list = parse(input);
     }
 
     public update(_: Scheduler) {
         if (this.state == State.PRE_ENTRY)
             this.state = State.ALIVE;
         var t = EntityPool.INSTANCE.special_effects.time_rate;
-        while (this.list.length > 0 && (t = this.list[0].update(t)))
-            this.list.shift();
+        while (this.list.length > 0 && t) {
+            var sss: ScheduleEntry | ScheduleSupplier = this.list[0];
+            if (sss instanceof ScheduleSupplier) {
+                var list = sss.supply();
+                if (!list || !list.length) {
+                    this.list.shift();
+                }
+                else this.list = [...list, ...this.list];
+            }
+            else {
+                t = sss.update(t);
+                if (t > 0)
+                    this.list.shift();
+            }
+        }
         if (this.list.length == 0)
             this.state = State.LEAVING;
     }
--- src/stg/test.ts
+++ src/stg/test.ts
@@ -1,15 +1,29 @@
 import { Bullet, template_config_bullet } from "./entity/Bullet";
-import { Scheduler } from "./schedule/Scheuler";
+import { Repeat, RepeatSupplier, Scheduler } from "./schedule/Scheuler";
 import { small_round_red } from "./sprite/sprites";
 import { EntityPool } from "./stage/EntityPool";
 
 const pool = new EntityPool();
 
-pool.add(new Scheduler([
-    120,
-    () => pool.add(new Bullet(small_round_red, template_config_bullet).simpleInit(0, 0, 1, 0)),
-    10
-]));
+const repeat = (item: Repeat, n: number = Infinity) => new RepeatSupplier(item, n);
+
+{
+    const n = 12;
+
+    pool.add(new Scheduler([
+        120,
+        repeat([
+            repeat((i) => [
+                () => pool.add(new Bullet(
+                    small_round_red,
+                    template_config_bullet)
+                    .simpleInit(0, 0, 1, Math.PI * 2 / n * i)),
+                10
+            ], n),
+            120
+        ], Infinity)
+    ]));
+}
 
 export function update() {
     pool.update();
