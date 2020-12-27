dee30ef6814b045f00f93524617611d89784afa9
lcy0x1 <arthurlcy0x1@gmail.com>
2020-12-27 06:44:51 -0800

    test load

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import * as stg from "./stg/test"
 import * as pf from "./platform/platform_init"
 
 async function main() {
+    console.log("window loaded");
     //pf.test_fps();
     pf.setup_canvas();
+    console.log("canvas setup");
     await stg.init();
+    console.log("stage initialized");
     require("./platform/buttons");
     pf.mainloop_start();
     window.debug_info.stg = {
